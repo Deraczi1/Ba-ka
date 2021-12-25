b3b4bf29c936446d597a2be8925342e712741d70
Deraczi <88276476+Deraczi1@users.noreply.github.com>
2021-12-25 17:07:49 +0100

    Add files via upload

---
+++ edit_baska/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "bodacious"
+games {"gta5"}
+
+name 'baska'
+author 'Deraczi'
+version '2.0'
+
+client_script {                                -- Client
+'client.lua',
+}
+
+server_script {
+'server.lua',
+}
